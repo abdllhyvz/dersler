ef9211a0ae8248a0f4b2ce53298ddb0e4e94afc2
Abdullah Yavuz <abdullahyavuz@Abdullahs-MacBook-Air.local>
2022-11-03 16:50:14 +0300

    ders 2

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,17 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int a = 3;
   String text = "bu bir string";
+  TextEditingController? textEditingController;
+  TextEditingController? passwordController;
+  bool isObscure = true;
+  int? i;
+
+  @override
+  void initState() {
+    super.initState();
+    textEditingController = TextEditingController();
+    passwordController = TextEditingController();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,20 +27,80 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(
-            "bu bir denemdir",
-            style: TextStyle(
-                fontSize: _height * .03,
-                color: Color.fromARGB(255, 144, 17, 167)),
-          ),
-          Text(
-            a.toString(),
-            style: TextStyle(fontSize: _width * .08),
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              //            mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  "Kullanıcı Adı:",
+                  style: TextStyle(fontSize: _height * .03),
+                ),
+                SizedBox(
+                  width: _width * 0.05,
+                ),
+                Container(
+                  width: _width * .6,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(25)))),
+                    controller: textEditingController,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: _height * .1,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Şifre Girin:",
+                  style: TextStyle(fontSize: _height * .03),
+                ),
+                SizedBox(
+                  width: _width * 0.05,
+                ),
+                Container(
+                  width: _width * .6,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(25)))),
+                    controller: passwordController,
+                    obscureText: isObscure,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: _height * .15,
+            ),
+            Container(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                ),
+                child: Text(
+                  "Giriş Yap",
+                  style: TextStyle(fontSize: 17),
+                ),
+                onPressed: () {
+                  setState(() {
+                    isObscure = !isObscure;
+                  });
+                },
+              ),
+              height: _height * .08,
+              width: _width * .3,
+            ),
+          ],
+        ),
       ),
     );
   }
