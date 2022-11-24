363656e328efce2468c10d7611f9fdeddb238b26
Abdullah Yavuz <abdullahyavuz@Abdullahs-MacBook-Air.local>
2022-11-24 19:06:26 +0300

    ders3

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:ders1/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,6 +14,7 @@ class _HomePageState extends State<HomePage> {
   TextEditingController? passwordController;
   bool isObscure = true;
   int? i;
+  IconData icon = Icons.remove_red_eye;
 
   @override
   void initState() {
@@ -26,82 +29,125 @@ class _HomePageState extends State<HomePage> {
     var _width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              //            mainAxisAlignment: MainAxisAlignment.spaceAround,
+        backgroundColor: Colors.grey[200],
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  "Kullanıcı Adı:",
-                  style: TextStyle(fontSize: _height * .03),
+                Container(
+                    height: _height * .25,
+                    width: _width * .6,
+                    child: Image(
+                      image: AssetImage("assets/logo.png"),
+                    )),
+                SizedBox(
+                  height: _height * .1,
+                ),
+                Row(
+                  //            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      "Kullanıcı Adı:",
+                      style: TextStyle(fontSize: _height * .03),
+                    ),
+                    SizedBox(
+                      width: _width * 0.05,
+                    ),
+                    Container(
+                      width: _width * .6,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(25)))),
+                        controller: textEditingController,
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(
-                  width: _width * 0.05,
+                  height: _height * .1,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Şifre Girin:",
+                      style: TextStyle(fontSize: _height * .03),
+                    ),
+                    SizedBox(
+                      width: _width * 0.05,
+                    ),
+                    Stack(
+                      children: [
+                        Container(
+                          width: _width * .6,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(25)))),
+                            controller: passwordController,
+                            obscureText: isObscure,
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(
+                              _width * .48, _width * .018, 0, 0),
+                          child: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isObscure = !isObscure;
+                                if (icon == Icons.remove_red_eye) {
+                                  icon = Icons.remove_red_eye_outlined;
+                                } else {
+                                  icon = Icons.remove_red_eye;
+                                }
+                              });
+                            },
+                            icon: Icon(icon),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: _height * .15,
                 ),
                 Container(
-                  width: _width * .6,
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(25)))),
-                    controller: textEditingController,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.red),
+                    ),
+                    child: Text(
+                      "Giriş Yap",
+                      style: TextStyle(fontSize: 17),
+                    ),
+                    onPressed: () {
+                      if (textEditingController!.text == "admin" &&
+                          passwordController!.text == "password") {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProfilePage()));
+                      } else {
+                        Fluttertoast.showToast(
+                            msg: "Kullanıcı Adı veya Şifreniz Hatalı!",
+                            toastLength: Toast.LENGTH_LONG,
+                            gravity: ToastGravity.CENTER,
+                            backgroundColor: Colors.white,
+                            textColor: Colors.black,
+                            fontSize: 16.0);
+                      }
+                    },
                   ),
+                  height: _height * .08,
+                  width: _width * .3,
                 ),
               ],
             ),
-            SizedBox(
-              height: _height * .1,
-            ),
-            Row(
-              children: [
-                Text(
-                  "Şifre Girin:",
-                  style: TextStyle(fontSize: _height * .03),
-                ),
-                SizedBox(
-                  width: _width * 0.05,
-                ),
-                Container(
-                  width: _width * .6,
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(25)))),
-                    controller: passwordController,
-                    obscureText: isObscure,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: _height * .15,
-            ),
-            Container(
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.red),
-                ),
-                child: Text(
-                  "Giriş Yap",
-                  style: TextStyle(fontSize: 17),
-                ),
-                onPressed: () {
-                  setState(() {
-                    isObscure = !isObscure;
-                  });
-                },
-              ),
-              height: _height * .08,
-              width: _width * .3,
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
---
+++ lib/profile_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        SystemNavigator.pop();
+        return true;
+      },
+      child: Scaffold(),
+    );
+  }
+}
